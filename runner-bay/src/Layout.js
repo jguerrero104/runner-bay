a814baab8de91f0aa4ed199ba1c33db631cfcd7a
jguerrero104 <jguerrero110761@gmail.com>
2024-03-06 16:18:20 -0600

    Added news feed scroll, updated layout, added rotues to other pages

--- runner-bay/src/Layout.js
+++ runner-bay/src/Layout.js
@@ -2,25 +2,26 @@ import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Dropdown from 'react-bootstrap/Dropdown';
+import { Link } from 'react-router-dom';
 
 const Layout = ({children}) => {
     return(
         <>
-         <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">RunnerBay</Navbar.Brand>
+        <Navbar bg="dark" variant="dark">
+        <Navbar.Brand as={Link} to="/">RunnerBay</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="#listings">Listings</Nav.Link>
-          <Nav.Link href="#news">News</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link as={Link} to="/listings">Listings</Nav.Link>
+          <Nav.Link as={Link} to="/news">News</Nav.Link>
+          <Nav.Link as={Link} to="/about">About</Nav.Link>
         </Nav>
         <Dropdown>
           <Dropdown.Toggle variant="dark">
             Profile
           </Dropdown.Toggle>
           <Dropdown.Menu variant="dark">
-            <Dropdown.Item href="#profile">View Profile</Dropdown.Item>
-            <Dropdown.Item href="#settings">Settings</Dropdown.Item>
-            <Dropdown.Item href="#logout">Logout</Dropdown.Item>
+            <Dropdown.Item as={Link} to="/profile">View Profile</Dropdown.Item>
+            <Dropdown.Item as={Link} to="/settings">Settings</Dropdown.Item>
+            <Dropdown.Item as={Link} to="/logout">Logout</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </Navbar>
