0476dec2643d608ce67097f01758c2a8f952ba47
jesus <jesus.pinales@my.utsa.edu>
2024-03-30 23:11:58 -0500

    only merge this lost and found

--- runner-bay/src/Home.js
+++ runner-bay/src/Home.js
@@ -1,27 +1,35 @@
-
 import React from 'react';
 import MarqueeSlider from 'react-marquee-slider';
 import times from 'lodash/times';
+import './Home.css';
 import { newsItems } from './News';
 
 const regularNewsItem = newsItems.filter(news => !news.spotlight);
 
 function Home() {
+  const openArticle = (url) => {
+    const win = window.open(url, '_blank');
+    if (win) {
+      win.focus();
+    } else {
+      alert('Please allow popups for this website');
+    }
+  };
+  
   return (
-    <div>
-      <MarqueeSlider velocity={5}>
+    <div className='home-news-slider'>
+      <MarqueeSlider velocity={20}>
         {regularNewsItem.map(news => (
-          <div key={`marquee-example-${news.id}`} style={{ width: "50%" }}>
+          <div key={`marquee-example-${news.id}`}  className='home-news-item' style={{ width: "70%" }}>
+            <img src={news.imageUrl} alt={news.title} onClick={() => openArticle(news.content)} />
             <h2>{news.title}</h2>
-            <p>{news.content}</p>
           </div>
         ))}
       </MarqueeSlider>
-      {
-        
-      }
     </div>
+
   );
 }
 
 export default Home;
+
